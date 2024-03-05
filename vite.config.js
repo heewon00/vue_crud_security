c656d937a9523e4d1cecd11952065f3a2bf3f158
heewon00 <55778040+heewon00@users.noreply.github.com>
2024-03-05 16:13:56 +0900

    240305 SCG

--- vite.config.js
+++ vite.config.js
@@ -18,16 +18,16 @@ export default defineConfig({
   ],
   //lcocal test 시 주석 삭제 
   // ---- Start -----
-  // server: {
-  //   host: 'localhost',
-  //   port: 3000,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8080',
-  //       changeOrigin: true
-  //     }
-  //   }
-  // },
+  server: {
+    host: 'localhost',
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true
+      }
+    }
+  },
   // ---- end -----
   //lcocal test 시 주석 삭제 
   resolve: {
